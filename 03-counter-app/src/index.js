b04b2b6d904a59bf0a2abcc8358662732c24ef17
mzunigau <mazuga92@gmail.com>
2021-05-24 20:46:11 -0600

    tarea counterapp

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp';
 
 import './index.css';
+import CounterApp from './CounterApp';
 
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( < PrimeraApp saludo = "Hola, soy Goku" / > , divRoot);
+// ReactDOM.render( < PrimeraApp saludo = "Hola, soy Goku" / > , divRoot);
+ReactDOM.render( < CounterApp value = {
+            10
+        }
+        /> , divRoot);
