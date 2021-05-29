595c602365b53d4c69ebd8f84914ef7d5e9cf934
mzunigau <mazuga92@gmail.com>
2021-05-28 19:29:27 -0600

    usando hooks

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(value);
 
   //handleAdd
   const handleAdd = () => {
@@ -14,6 +14,20 @@ const CounterApp = ({ value }) => {
     <>
       <h1> CounterApp </h1> <h2> {counter} </h2>
       <button onClick={handleAdd}>+1</button>
+      <button
+        onClick={() => {
+          setCounter(value);
+        }}
+      >
+        Reset
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        -1
+      </button>
     </>
   );
 };
