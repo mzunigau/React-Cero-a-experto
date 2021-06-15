18f77c5ec758862f87a2e4cb9e979bcd915c3cb6
mzunigau <mazuga92@gmail.com>
2021-06-14 19:26:55 -0600

    trabajando con promesas

---
+++ 03-counter-app/src/tests/base/09-promesas.test.js
@@ -0,0 +1,25 @@
+import {
+    getHeroeByIdAsync
+} from "../../base/09-promesas";
+import heroes from "../../data/heroes";
+
+describe('Pruebas con promesas', () => {
+
+    test('debe de retornar un heroe async', (done) => {
+
+        const id = 1;
+
+
+        getHeroeByIdAsync(id).then(heroe => {
+
+            expect(heroe).toBe(heroes[0]);
+            done();
+
+        });
+
+
+
+    });
+
+
+});
